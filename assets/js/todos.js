dacca646ec67b51f432e663b40559c598399ff1e
Steve <anonymouse@bestaatniet.com>
2019-08-04 13:33:18 +0200

    Na functioneel werkend maken

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,23 @@
-alert("CONNECTED")
+//Check Off Specific Todos By Clicking
+$(".js-list").on("click", ".js-do", function() {
+    $(this).toggleClass("todo__do--completed")
+});
+
+//Click on X to delete todo
+$(".js-list").on("click", ".js-delete", function(event) {
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+//add todo after enter
+$(".js-input").on("keypress", function(event) {
+    if (event.which === 13) {
+        //grabbing new todo text
+        var todoText = $(this).val();
+        $(this).val("")
+        //create new li and add to ul
+        $(".js-list").append("<li class=\"js-do\"><span class=\"js-delete\">X</span> " + todoText + "</li>")
+    }
+});
